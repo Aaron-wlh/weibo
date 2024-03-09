cc769aa616a5d6c8e288ed12d8a262d046081a00
wlh <976724250@qq.com>
2024-03-09 09:03:37 +0000

    中文的错误提示信息

---
+++ resources/lang/zh-CN/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'required'             => ':attribute 不能为空。',
+    'unique'      =>    ':attribute 存在咯，换一个吧。',
+    'attributes' => [
+        'email'                 => '邮箱地址',
+    ]
+
+];
