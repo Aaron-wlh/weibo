2565f359d49266e65976d2abe0e9ca48c92eb34e
wlh <976724250@qq.com>
2024-03-10 05:10:10 +0000

    会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        $user = User::query()->where('email', $request->input('email'))->first();
+        if (!$user) {
+            session()->flash('danger', '该邮箱不存在');
+            return redirect()->back()->withInput();
+        }
+        if (Hash::check($user->password, $request->input('password'))) {
+            session()->flash('danger', '您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+
+        Auth::login($user);
+
+        session()->flash('success', '欢迎回来！');
+        return redirect()->route('users.show', ['user' => Auth::user()]);
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect()->route('home');
+    }
+}
